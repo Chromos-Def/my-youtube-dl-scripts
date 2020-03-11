f76600dd6a92857da42b6d1dcaf091424e7b3f4c
Chromos-Def <dewp20x@gmail.com>
2020-03-11 04:40:33 -0400

    renamed url

--- 1-Convert-To-VIDEO.bat
+++ 1-Convert-To-VIDEO.bat
@@ -1,4 +1,4 @@
-powershell -command "Get-Clipboard" > youtube-url.txt
+powershell -command "Get-Clipboard" > dl-url.txt
 for /f "delims=" %%x in (youtube-url.txt) do set content=%%x
 youtube-dl.exe %content% --geo-bypass -f bestvideo+bestaudio --merge-output-format mkv --metadata-from-title "(?P<artist>.+?) - (?P<title>.+)" -o "%%(title)s.%%(ext)s"
 pause
